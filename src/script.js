f82a47c77eb5c48f96fb3943706b6cd5fdad3aa5
Karim Hatem <119761193+karimhatemhosny@users.noreply.github.com>
2023-08-29 17:48:27 +0300

    fixed a bug where it couldn't display 8

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
 let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-function rand(max){
-	return Math.ceil(Math.random() * max)
+function rand(min, max){
+	return Math.floor(Math.random() * max) + min
 }
 
 function doThingy(){
   let a = 1
   let b = 1
 
-  a = rand(7)
-  b = rand(7)
+  a = rand(0,8)
+  b = rand(1,8)
   console.log(arr[a]+b)
   document.querySelector('#square').innerText = arr[a]
   document.querySelector('#square').innerText += b
